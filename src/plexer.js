bf30f7424a6cb02f6b42caa6e781bc0c0c87906d
Charlie Windolf <cwindolf95@gmail.com>
2014-07-21 11:07:01 -0400

    v1.1
    add strict mode, run on document.ready, remove extra script tag in
    header for initialization

--- src/plexer.js
+++ src/plexer.js
@@ -1,19 +1,24 @@
-var plexer = {};
+$(function() {
+    'use strict';
+    var plexer = {};
 
-plexer.activate = function(data_content_name) {
-    $('.plexer #switchboard li').removeClass('active');
-    $('.plexer #content div').removeClass('active');
-    $('*[data-content-name="' + data_content_name + '"]').addClass('active');
-    $('*[data-name="' + data_content_name + '"').addClass('active');
-};
+    plexer.activate = function(data_content_name) {
+        $('.plexer #switchboard li').removeClass('active');
+        $('.plexer #content div').removeClass('active');
+        $('li[data-content-name="' + data_content_name + '"]').addClass('active');
+        $('div[data-name="' + data_content_name + '"]').addClass('active');
+    };
 
-plexer.new = function() {
-    $('.plexer #switchboard li').each(function(i) {
-        $(this).click(function(e) {
-            e.preventDefault();
-            plexer.activate($(this).attr('data-content-name'));
-            return false;
+    plexer.new = function() {
+        $('.plexer #switchboard li').each(function(i) {
+            $(this).click(function(e) {
+                e.preventDefault();
+                plexer.activate($(this).attr('data-content-name'));
+                return false;
+            });
         });
-    });
-    plexer.activate($('.plexer #switchboard li:first-child').attr('data-content-name'));
-};
+        plexer.activate($('.plexer #switchboard li:first-child').attr('data-content-name'));
+    };
+
+    plexer.new();
+});
