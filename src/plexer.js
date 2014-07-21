e448347f76c62bd530b8bef2ed12c307007b9562
Charlie Windolf <cwindolf95@gmail.com>
2014-07-20 22:39:24 -0400

    v1
    expects a dom structure. open example.html in a browser for details.

---
+++ src/plexer.js
@@ -0,0 +1,19 @@
+var plexer = {};
+
+plexer.activate = function(data_content_name) {
+    $('.plexer #switchboard li').removeClass('active');
+    $('.plexer #content div').removeClass('active');
+    $('*[data-content-name="' + data_content_name + '"]').addClass('active');
+    $('*[data-name="' + data_content_name + '"').addClass('active');
+};
+
+plexer.new = function() {
+    $('.plexer #switchboard li').each(function(i) {
+        $(this).click(function(e) {
+            e.preventDefault();
+            plexer.activate($(this).attr('data-content-name'));
+            return false;
+        });
+    });
+    plexer.activate($('.plexer #switchboard li:first-child').attr('data-content-name'));
+};
